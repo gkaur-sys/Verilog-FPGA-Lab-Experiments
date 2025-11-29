05a5fd2b35f4ee67e94c45212816ea73ad4e46fb
Gunpreet Kaur <gunpreetkaur07@gmail.com>
2025-11-29 11:40:18 +0530

    Update full_adder.v

--- 03_Ripple_Carry_Adder/full_adder.v
+++ 03_Ripple_Carry_Adder/full_adder.v
@@ -1,1 +1,13 @@
-
+`timescale 1ns/1ps
+module full_adder(
+    input  wire a,
+    input  wire b,
+    input  wire cin,
+    output wire sum,
+    output wire cout
+);
+   
+    assign sum  = a ^ b ^ cin;
+    
+    assign cout = (a & b) | (a & cin) | (b & cin);
+endmodule
