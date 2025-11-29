f39b1226670cfb6ca03b5957017c3b39f33d7859
Gunpreet Kaur <gunpreetkaur07@gmail.com>
2025-11-29 15:19:54 +0530

    Update alu.v

--- 04_ALU_4bit/alu.v
+++ 04_ALU_4bit/alu.v
@@ -1,1 +1,34 @@
+module tb(
+    );
+    
+reg [3:0] a,b;
+reg [2:0] s;
+wire [3:0] f;
 
+alu dut (
+    .a(a),
+    .b(b),
+    .s(s),
+    .f(f)
+    
+);
+
+initial begin 
+    // giving the values of A and B here is optional 
+    a = 4'b1010;
+    b = 4'b0011;
+    
+    s = 3'b000; #10;
+    s = 3'b001; #10;
+    s = 3'b010; #10;
+    s = 3'b011; #10;
+    s = 3'b100; #10;
+    s = 3'b101; #10;
+    s = 3'b110; #10;
+    s = 3'b111; #10;
+    
+    $stop;
+ end
+    
+    
+endmodule
